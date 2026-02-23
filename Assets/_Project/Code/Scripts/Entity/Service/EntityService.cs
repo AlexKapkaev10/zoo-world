04663228606118ff8618e1be3a7d245734719f71
AlexKapkaev10 <chivoda10@gmail.com>
2026-02-23 15:51:30 +0300

    update CameraViewportModel add _pendingRemove collection for code sequrity, update EntityFactory namings

--- Assets/_Project/Code/Scripts/Entity/Service/EntityService.cs
+++ Assets/_Project/Code/Scripts/Entity/Service/EntityService.cs
@@ -22,12 +22,27 @@ namespace Project.Entities
         private readonly ICameraService _cameraService;
         private readonly IDisposable _eatPreySubscription;
 
+        private readonly List<PendingDespawn> _pendingDespawn = new();
+        private bool _isTicking;
+
+        private readonly struct PendingDespawn
+        {
+            public readonly IEntity Entity;
+            public readonly bool ReleaseToPool;
+
+            public PendingDespawn(IEntity entity, bool releaseToPool)
+            {
+                Entity = entity;
+                ReleaseToPool = releaseToPool;
+            }
+        }
+
         [Inject]
         public EntityService(IGameScopeFactory factory, EntityServiceConfig config)
         {
             _config = config;
             _cameraService = factory.Get<ICameraService>();
-            
+
             _pool = new EntityPool(factory.Get<IEntityFactory>());
             _spawnModel = new SpawnEntityModel(_config);
 
@@ -40,7 +55,7 @@ namespace Project.Entities
         {
             message.Killed.StartDeath();
         }
-        
+
         public void Start()
         {
             _pool.Prewarm(_config.SpawnData);
@@ -49,18 +64,28 @@ namespace Project.Entities
 
         public void Tick()
         {
+            _isTicking = true;
+
             foreach (var entity in _entityMap.Keys)
             {
                 entity.Tick();
             }
+
+            _isTicking = false;
+            FlushPendingDespawn();
         }
 
         public void FixedTick()
         {
+            _isTicking = true;
+
             foreach (var entity in _entityMap.Keys)
             {
                 entity.FixedTick();
             }
+
+            _isTicking = false;
+            FlushPendingDespawn();
         }
 
         public void Dispose()
@@ -77,14 +102,16 @@ namespace Project.Entities
             }
         }
 
-        private void SpawnEntity(SpawnArchetypeData spawnData, Vector3 spawnPosition, Quaternion bodyRotation)
+        private void SpawnEntity(SpawnArchetypeData spawnData, 
+            Vector3 spawnPosition, 
+            Quaternion bodyRotation)
         {
             var entity = _pool.Get(spawnData.Archetype);
             entity.Spawn(spawnPosition, bodyRotation);
 
             entity.Deactivated += OnEntityDeactivated;
             entity.Destroyed += OnEntityDestroy;
-            
+
             _entityMap[entity] = spawnData;
             _spawnModel.RegisterSpawn(spawnData.Archetype.Data.Kind);
             _cameraService.AddViewportObserved(entity);
@@ -103,7 +130,7 @@ namespace Project.Entities
             entity.Destroyed -= OnEntityDestroy;
             _cameraService.RemoveViewportObserved(entity);
 
-            if (releaseToPool && _pool != null)
+            if (releaseToPool)
             {
                 _pool.Release(entity);
             }
@@ -111,26 +138,56 @@ namespace Project.Entities
 
         private void OnEntityDestroy(IEntity entity)
         {
-            DespawnEntity(entity, releaseToPool: false);
+            RequestDespawn(entity, releaseToPool: false);
         }
 
         private void OnEntityDeactivated(IEntity entity)
         {
-            DespawnEntity(entity, releaseToPool: true);
+            RequestDespawn(entity, releaseToPool: true);
+        }
+
+        private void RequestDespawn(IEntity entity, bool releaseToPool)
+        {
+            if (!_isTicking)
+            {
+                DespawnEntity(entity, releaseToPool);
+                return;
+            }
+
+            _pendingDespawn.Add(new PendingDespawn(entity, releaseToPool));
+        }
+
+        private void FlushPendingDespawn()
+        {
+            if (_pendingDespawn.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var pending in _pendingDespawn)
+            {
+                DespawnEntity(pending.Entity, pending.ReleaseToPool);
+            }
+
+            _pendingDespawn.Clear();
         }
 
         private async UniTaskVoid SpawnAsync(CancellationToken token)
         {
             while (!token.IsCancellationRequested)
             {
-                if (_spawnModel.TryGetSpawnRequest(out var spawnData, out var spawnPosition, out var bodyRotation))
+                if (_spawnModel.TryGetSpawnRequest(
+                        out var spawnData, 
+                        out var spawnPosition, 
+                        out var bodyRotation))
                 {
                     SpawnEntity(spawnData, spawnPosition, bodyRotation);
                 }
 
-                await UniTask.Delay(TimeSpan.FromSeconds(_config.SpawnIntervalSeconds), cancellationToken: token);
+                await UniTask.Delay(TimeSpan.FromSeconds(
+                    _config.SpawnIntervalSeconds), 
+                    cancellationToken: token);
             }
         }
-
     }
 }
